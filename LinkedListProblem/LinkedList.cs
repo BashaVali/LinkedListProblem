59155d55df8fa26935acc8b3bebbf06502878761
BashaVali <bashavali222@gmail.com>
2022-11-21 02:35:10 +0530

    Linked List Problem

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+        public class LinkedList
+        {
+        //Creating a head node to point the first element
+           public Node head;
+        //Creating a method to insert last
+        public void Add(int data)
+            {
+                Node node = new Node(data);
+                if (this.head == null)
+                {
+                    this.head = node;
+                //return;
+                }
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                Console.WriteLine("{0} inserted into LinkedList", node.data);
+            }
+            //Display the nodes
+            public void Display()
+            {
+                Node temp = this.head;
+                if (temp == null)
+                {
+                    Console.WriteLine("LinkedList is empty");
+                }
+                Console.Write("LinkedList is as : ");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+
+            }
+        }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,41 @@
+﻿using LinkedListProblem;
+using System;
+namespace LinkedListStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Linked List Problem " + "\n\n");
+
+                Console.WriteLine(" Select the option ");
+
+                Console.WriteLine("1.CreateLinkedList\n" +
+                                  "2.Display" + "\n" +
+                                  
+                                  "9.Exit" + "\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Display();
+                        break;
+                    case 2:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
